1456dd0e7eb62b43ae5fff7809ee775a452d9954
Anusha R <133990109+Anusha-r03@users.noreply.github.com>
2024-09-26 16:38:22 +0000

    DSA Sheet AC

--- MinMaxElemInArray.java
+++ MinMaxElemInArray.java
@@ -58,47 +58,113 @@
 //     }
 // }
 
-// Maximum and minimum of an array using Linear search:
-// Time Complexity: O(n)
-// Auxiliary Space: O(1) as no extra space was needed.
-class Pair{
-    public int min;
-    public int max;
-}
-class MinMaxElemInArray{
+// // Maximum and minimum of an array using Linear search:
+// // Time Complexity: O(n)
+// // Auxiliary Space: O(1) as no extra space was needed.
+// class Pair{
+//     public int min;
+//     public int max;
+// }
+// class MinMaxElemInArray{
 
-  public static Pair getMinMax(int arr[], int n){
+//   public static Pair getMinMax(int arr[], int n){
 
-    Pair minMax = new Pair();
-    if(n == 1){
-        minMax.min = arr[0];
-        minMax.max = arr[0];
-    }
-    else if(arr[0]<arr[1]){
-        minMax.min = arr[0];
-        minMax.max = arr[1];
-    }
-    else{
-        minMax.min = arr[1];
-        minMax.max = arr[0];
+//     Pair minMax = new Pair();
+//     if(n == 1){
+//         minMax.min = arr[0];
+//         minMax.max = arr[0];
+//         return minMax;
+//     }
+//     else if(arr[0]<arr[1]){
+//         minMax.min = arr[0];
+//         minMax.max = arr[1];
+//     }
+//     else{
+//         minMax.min = arr[1];
+//         minMax.max = arr[0];
+//     }
+
+//     for(int i=2; i<n; i++){
+//         if(arr[i]<minMax.min){
+//             minMax.min = arr[i];
+//         }
+//         else if(arr[i]>minMax.max){
+//             minMax.max = arr[i];
+//         }
+//     }  
+//     return minMax;
+//   }
+
+//   public static void main(String[] args){
+//       int[] arr = {100, 87, 65, 10, 36};
+//       int n = arr.length;
+//       Pair minMax = getMinMax(arr, n);
+//       System.out.println("Minimum Element: "+ minMax.min);
+//       System.out.println("Maximum Element: "+ minMax.max);
+//   }
+// }
+
+
+// Java program of above implementation
+public class MinMaxElemInArray {
+
+    static class Pair {
+
+        int min;
+        int max;
     }
 
-    for(int i=2; i<n; i++){
-        if(arr[i]<minMax.min){
-            minMax.min = arr[i];
+    static Pair getMinMax(int arr[], int n) {
+        Pair minmax = new Pair();
+        int i;
+        /* If array has even number of elements then initialize the first two elements as minimum and maximum */
+        if (n % 2 == 0) {
+            if (arr[0] > arr[1]) {
+                minmax.max = arr[0];
+                minmax.min = arr[1];
+            } else {
+                minmax.min = arr[0];
+                minmax.max = arr[1];
+            }
+            i = 2;
+            /* set the starting index for loop */
+        } /* If array has odd number of elements then initialize the first element as minimum and maximum */ 
+        else {
+            minmax.min = arr[0];
+            minmax.max = arr[0];
+            i = 1;
+            /* set the starting index for loop */
         }
-        else if(arr[i]>minMax.max){
-            minMax.max = arr[i];
-        }
-    }  
-    return minMax;
-  }
 
-  public static void main(String[] args){
-      int[] arr = {100, 87, 65, 10, 36};
-      int n = arr.length;
-      Pair minMax = getMinMax(arr, n);
-      System.out.println("Minimum Element: "+ minMax.min);
-      System.out.println("Maximum Element: "+ minMax.max);
-  }
+        /* In the while loop, pick elements in pair and compare the pair with max and min so far */
+        while (i < n - 1) {
+            if (arr[i] > arr[i + 1]) {
+                if (arr[i] > minmax.max) {
+                    minmax.max = arr[i];
+                }
+                if (arr[i + 1] < minmax.min) {
+                    minmax.min = arr[i + 1];
+                }
+            } else {
+                if (arr[i + 1] > minmax.max) {
+                    minmax.max = arr[i + 1];
+                }
+                if (arr[i] < minmax.min) {
+                    minmax.min = arr[i];
+                }
+            }
+            i += 2;
+            /* Increment the index by 2 as two elements are processed in loop */
+        }
+        return minmax;
+    }
+    /* Driver program to test above function */
+    public static void main(String args[]) {
+        int arr[] = {1000, 11, 445, 1, 330, 3000};
+        int arr_size = 6;
+        Pair minmax = getMinMax(arr, arr_size);
+        System.out.printf("Minimum element is %d", minmax.min);
+        System.out.printf("\nMaximum element is %d", minmax.max);
+
+    }
 }
